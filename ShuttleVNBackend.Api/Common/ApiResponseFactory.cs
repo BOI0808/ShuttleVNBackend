259e68f9f2173467dc8e5417359ad44b61c43588
vietng <vietng322611@gmail.com>
2026-09-15 10:54:50 +0700

    introduce `ApiResponse` warper

---
+++ ShuttleVNBackend.Api/Common/ApiResponseFactory.cs
@@ -0,0 +1,14 @@
+﻿using ShuttleVNBackend.Api.DTOs.Common;
+
+namespace ShuttleVNBackend.Api.Common;
+
+public class ApiResponseFactory
+{
+    public static ApiResponse<T> Success<T>(T data)
+        => new(true, data, null, null);
+
+    public static ApiResponse<T> Failure<T>(
+        string message,
+        IDictionary<string, string[]>? errors = null)
+        => new(false, default, message, errors);
+}
