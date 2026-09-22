af3215971219a930ccf7317769c7491b84cb016b
BOI0808 <nhatkhoi30@gmail.com>
2026-09-22 19:27:47 +0700

    refactor(repository): remove sequence generator and revert employee query

--- ShuttleVNBackend.Application/Interfaces/Repositories/IEmployeeRepository.cs
+++ ShuttleVNBackend.Application/Interfaces/Repositories/IEmployeeRepository.cs
@@ -9,5 +9,4 @@ public interface IEmployeeRepository
     Task<UserAccount?> GetByIdAsync(Guid id, CancellationToken ct = default);
     Task<UserAccount?> GetByAccountIdAsync(Guid accountId, CancellationToken ct = default);
     Task<Employee?> GetByEmailAsync(string email, CancellationToken ct = default);
-    Task<int> GetNextDeletedSequenceAsync(CancellationToken ct = default);
 }
