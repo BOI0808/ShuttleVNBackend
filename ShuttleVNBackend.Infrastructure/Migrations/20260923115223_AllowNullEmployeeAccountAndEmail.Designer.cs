b45f8589b4fcc26b81021da62ced2a17f8c00cef
BOI0808 <nhatkhoi30@gmail.com>
2026-09-23 19:19:32 +0700

    feat(database): add migration AllowNullEmployeeAccountAndEmail

--- ShuttleVNBackend.Infrastructure/Migrations/20260923115223_AllowNullEmployeeAccountAndEmail.Designer.cs
+++ ShuttleVNBackend.Infrastructure/Migrations/20260923115223_AllowNullEmployeeAccountAndEmail.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShuttleVNBackend.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using ShuttleVNBackend.Infrastructure.Persistence;
 namespace ShuttleVNBackend.Infrastructure.Migrations
 {
     [DbContext(typeof(ShuttleVnDbContext))]
-    partial class ShuttleVnDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260923115223_AllowNullEmployeeAccountAndEmail")]
+    partial class AllowNullEmployeeAccountAndEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
